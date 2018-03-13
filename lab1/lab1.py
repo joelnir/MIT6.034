33a12f8e28515a9a93554ff9211483ed572dfd68
joelnir <joel.oscarsson@hotmail.com>
2018-03-13 16:12:41 +0100

    Implement forward chaining part of lab1.

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,4 +1,4 @@
-# lab1.py 
+# lab1.py
 
 #You should start here when providing the answers to Problem Set 1.
 #Follow along in the problem set, which is at:
@@ -17,7 +17,7 @@ from production import IF, AND, OR, NOT, THEN, forward_chain
 #    2. the consequent
 #    3. both
 
-ANSWER_1 = 'your answer here'
+ANSWER_1 = '2'
 
 # A rule-based system about Monty Python's "Dead Parrot" sketch
 # uses the following rules:
@@ -37,10 +37,10 @@ ANSWER_1 = 'your answer here'
 
 # Will this system produce the datum 'Polly is pining for the
 # fjords'?  Answer 'yes' or 'no'.
-ANSWER_2 = 'your answer here'
+ANSWER_2 = 'no'
 
 # Which rule contains a programming error? Answer '1' or '2'.
-ANSWER_3 = 'your answer here'
+ANSWER_3 = '2'
 
 # If you're uncertain of these answers, look in tests.py for an
 # explanation.
@@ -69,11 +69,11 @@ ANSWER_3 = 'your answer here'
 # what is asked.  After we start the system running, which rule
 # fires first?
 
-ANSWER_4 = 'your answer here'
+ANSWER_4 = '1'
 
 # Which rule fires second?
 
-ANSWER_5 = 'your answer here'
+ANSWER_5 = '0'
 
 
 # Problem 1.3.1: Poker hands
@@ -90,7 +90,7 @@ poker_data = ( 'two-pair beats pair',
 # which poker hands beat which, transitively. For example, it
 # should be able to deduce that a three-of-a-kind beats a pair,
 # because a three-of-a-kind beats two-pair, which beats a pair.
-transitive_rule = IF( AND(), THEN() )
+transitive_rule = IF( AND('(?x) beats (?y)', '(?y) beats (?z)'), THEN('(?x) beats (?z)') )
 
 # You can test your rule like this:
 # print forward_chain([transitive_rule], poker_data)
@@ -100,8 +100,8 @@ transitive_rule = IF( AND(), THEN() )
 TEST_RESULTS_TRANS1 = forward_chain([transitive_rule],
                                     [ 'a beats b', 'b beats c' ])
 TEST_RESULTS_TRANS2 = forward_chain([transitive_rule],
-  [ 'rock beats scissors', 
-    'scissors beats paper', 
+  [ 'rock beats scissors',
+    'scissors beats paper',
     'paper beats rock' ])
 
 
@@ -112,9 +112,33 @@ TEST_RESULTS_TRANS2 = forward_chain([transitive_rule],
 # able to refer to the rules by name and easily rearrange them if
 # you need to.
 
+same_rule = IF(OR('male (?x)', 'female (?x)'), THEN('same-identity (?x) (?x)'))
+sibling_rule = IF(AND('parent (?x) (?y)', 'parent (?x) (?z)', NOT('same-identity (?y) (?z)')), THEN('sibling (?y) (?z)'))
+brother_rule = IF(AND('sibling (?x) (?y)', 'male (?x)'), THEN('brother (?x) (?y)'))
+sister_rule = IF(AND('sibling (?x) (?y)', 'female (?x)'), THEN('sister (?x) (?y)'))
+mother_rule = IF(AND('parent (?x) (?y)', 'female (?x)'), THEN('mother (?x) (?y)'))
+father_rule = IF(AND('parent (?x) (?y)', 'male (?x)'), THEN('father (?x) (?y)'))
+son_rule = IF(AND('parent (?y) (?x)', 'male (?x)'), THEN('son (?x) (?y)'))
+daughter_rule = IF(AND('parent (?y) (?x)', 'female (?x)'), THEN('daughter (?x) (?y)'))
+cousin_rule = IF(AND('parent (?z) (?x)', 'parent (?w) (?y)', 'sibling (?z) (?w)'), THEN('cousin (?x) (?y)'))
+grandparent_rule = IF(AND('parent (?x) (?y)', 'parent (?y) (?z)'), THEN('grandparent (?x) (?z)'))
+grandchild_rule = IF(AND('grandparent (?x) (?y)'), THEN('grandchild (?y) (?x)'))
+
 # Then, put them together into a list in order, and call it
 # family_rules.
-family_rules = [ ]                    # fill me in
+family_rules = [
+    same_rule,
+    sibling_rule,
+    brother_rule,
+    sister_rule,
+    mother_rule,
+    father_rule,
+    son_rule,
+    daughter_rule,
+    cousin_rule,
+    grandparent_rule,
+    grandchild_rule
+ ]
 
 # Some examples to try it on:
 # Note: These are used for testing, so DO NOT CHANGE
@@ -161,13 +185,13 @@ black_data = ("male sirius",
 # This should generate 14 cousin relationships, representing
 # 7 pairs of people who are cousins:
 
-black_family_cousins = [ 
-    x for x in 
-    forward_chain(family_rules, black_data, verbose=False) 
+black_family_cousins = [
+    x for x in
+    forward_chain(family_rules, black_data, verbose=False)
     if "cousin" in x ]
 
 # To see if you found them all, uncomment this line:
-# print black_family_cousins
+# print len(black_family_cousins)
 
 # To debug what happened in your rules, you can set verbose=True
 # in the function call above.
@@ -180,12 +204,12 @@ TEST_DATA_1 = [ 'female alice',
                 'male chuck',
                 'parent chuck alice',
                 'parent chuck bob' ]
-TEST_RESULTS_1 = forward_chain(family_rules, 
+TEST_RESULTS_1 = forward_chain(family_rules,
                                TEST_DATA_1, verbose=False)
 
-TEST_DATA_2 = [ 'female a1', 'female b1', 'female b2', 
-                'female c1', 'female c2', 'female c3', 
-                'female c4', 'female d1', 'female d2', 
+TEST_DATA_2 = [ 'female a1', 'female b1', 'female b2',
+                'female c1', 'female c2', 'female c3',
+                'female c4', 'female d1', 'female d2',
                 'female d3', 'female d4',
                 'parent a1 b1',
                 'parent a1 b2',
@@ -198,7 +222,7 @@ TEST_DATA_2 = [ 'female a1', 'female b1', 'female b2',
                 'parent c3 d3',
                 'parent c4 d4' ]
 
-TEST_RESULTS_2 = forward_chain(family_rules, 
+TEST_RESULTS_2 = forward_chain(family_rules,
                                TEST_DATA_2, verbose=False)
 
 TEST_RESULTS_6 = forward_chain(family_rules,
@@ -216,4 +240,3 @@ from backchain import backchain_to_goal_tree
 HOW_MANY_HOURS_THIS_PSET_TOOK = ''
 WHAT_I_FOUND_INTERESTING = ''
 WHAT_I_FOUND_BORING = ''
-
