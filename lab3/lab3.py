c2e1ab36d1b5a79da133a0213579a330015f916e
joelnir <joel.oscarsson@hotmail.com>
2018-03-15 17:11:29 +0100

    Finish lab 3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,6 +1,4 @@
 # 6.034 Fall 2010 Lab 3: Games
-# Name: <Your Name>
-# Email: <Your Email>
 
 from util import INFINITY
 
@@ -14,7 +12,7 @@ from util import INFINITY
 #      1. MM will play better than AB.
 #      2. AB will play better than MM.
 #      3. They will play with the same level of skill.
-ANSWER1 = 0
+ANSWER1 = 3
 
 # 1.2. Two computerized players are playing a game with a time limit. Player MM
 # does minimax search with iterative deepening, and player AB does alpha-beta
@@ -24,7 +22,7 @@ ANSWER1 = 0
 #   1. MM will play better than AB.
 #   2. AB will play better than MM.
 #   3. They will play with the same level of skill.
-ANSWER2 = 0
+ANSWER2 = 2
 
 ### 2. Connect Four
 from connectfour import *
@@ -36,7 +34,7 @@ import tree_searcher
 ## the game interactively. Be sure to re-comment them when you're done with
 ## them.  Please don't turn in a problem set that sits there asking the
 ## grader-bot to play a game!
-## 
+##
 ## Uncomment this line to play a game as white:
 #run_game(human_player, basic_player)
 
@@ -56,8 +54,26 @@ def focused_evaluate(board):
     that board is for the current player.
     A return value >= 1000 means that the current player has won;
     a return value <= -1000 means that the current player has lost
-    """    
-    raise NotImplementedError
+    """
+    if board.is_game_over():
+        # If the game has been won, we know that it must have been
+        # won or ended by the previous move.
+        # The previous move was made by our opponent.
+        # Therefore, we can't have won, so return -1000.
+        # (note that this causes a tie to be treated like a loss)
+        score = - (1000 + (1000/board.num_tokens_on_board()))
+    else:
+        score = board.longest_chain(board.get_current_player_id()) * 10
+        # Prefer having your pieces in the center of the board.
+        for row in range(6):
+            for col in range(7):
+                if board.get_cell(row, col) == board.get_current_player_id():
+                    score -= abs(3-col)
+                elif board.get_cell(row, col) == board.get_other_player_id():
+                    score += abs(3-col)
+
+    return score
+
 
 
 ## Create a "player" function that uses the focused_evaluate function
@@ -73,16 +89,44 @@ quick_to_win_player = lambda board: minimax(board, depth=4,
 ## counting the number of static evaluations you make.
 ##
 ## You can use minimax() in basicplayer.py as an example.
+
+# NOTE: You should use get_next_moves_fn when generating
+# next board configurations, and is_terminal_fn when
+# checking game termination.
+# The default functions set here will work
+# for connect_four.
 def alpha_beta_search(board, depth,
                       eval_fn,
-                      # NOTE: You should use get_next_moves_fn when generating
-                      # next board configurations, and is_terminal_fn when
-                      # checking game termination.
-                      # The default functions set here will work
-                      # for connect_four.
                       get_next_moves_fn=get_all_next_moves,
-		      is_terminal_fn=is_terminal):
-    raise NotImplementedError
+        		      is_terminal_fn=is_terminal,
+                      alpha = NEG_INFINITY,
+                      beta = INFINITY,
+                      top = True):
+    if(is_terminal_fn(depth, board)):
+        return eval_fn(board)
+
+    chosen_move = None
+
+    for move, new_board in get_next_moves_fn(board):
+        val = -1 * alpha_beta_search(new_board, depth-1, eval_fn,
+                                            get_next_moves_fn,
+                                            is_terminal_fn,
+                                            -beta,
+                                            -alpha,
+                                            False)
+
+        if(val > alpha):
+            alpha = val
+            chosen_move = move
+
+        if(alpha >= beta):
+            return alpha
+
+    if(top):
+        #print "A-B: Decided on column %d with rating %d" % (chosen_move, alpha)
+        return chosen_move
+    else:
+        return alpha
 
 ## Now you should be able to search twice as deep in the same amount of time.
 ## (Of course, this alpha-beta-player won't work until you've defined
@@ -97,7 +141,8 @@ ab_iterative_player = lambda board: \
     run_search_function(board,
                         search_fn=alpha_beta_search,
                         eval_fn=focused_evaluate, timeout=5)
-#run_game(human_player, alphabeta_player)
+#run_game(human_player, ab_iterative_player)
+#run_game(alphabeta_player, alphabeta_player)
 
 ## Finally, come up with a better evaluation function than focused-evaluate.
 ## By providing a different function, you should be able to beat
@@ -105,10 +150,50 @@ ab_iterative_player = lambda board: \
 ## same depth.
 
 def better_evaluate(board):
-    raise NotImplementedError
+    my_id = board.get_current_player_id()
+    opp_id = board.get_other_player_id()
+
+    if(board.is_win() == my_id):
+        return 1000
+    elif(board.is_win() == opp_id):
+        return -1000
+    else:
+        #ongoing state
+        score = 0
+
+        if(board.num_tokens_on_board() == 0):
+            return 0
+
+        # chain scoring
+        my_chains = board.chain_cells(my_id)
+        my_chain_lengths = [len(chain) for chain in my_chains]
+        opp_chains = board.chain_cells(opp_id)
+        opp_chain_lengths = [len(chain) for chain in opp_chains]
+
+        if(len(my_chain_lengths) > 0):
+            score += reduce((lambda x, y: x + y), my_chain_lengths)
+        if(len(opp_chain_lengths) > 0):
+            score -= reduce((lambda x, y: x + y), opp_chain_lengths)
+
+        #prioritize center
+        for x in range(2, 5, 1):
+            for y in range(6):
+                cell_id = board.get_cell(y, x)
+
+                if (cell_id == my_id):
+                    score += 1
+                elif (cell_id == opp_id):
+                    score -= 1
+
+        # Remove score based on length of game
+        score = (score * 10/ (board.num_tokens_on_board()))
+        # * 10 to make sure we don't always end up with 1
+
+        return score
+
 
 # Comment this line after you've fully implemented better_evaluate
-better_evaluate = memoize(basic_evaluate)
+better_evaluate = memoize(better_evaluate)
 
 # Uncomment this line to make your better_evaluate run faster.
 # better_evaluate = memoize(better_evaluate)
@@ -116,10 +201,10 @@ better_evaluate = memoize(basic_evaluate)
 # For debugging: Change this if-guard to True, to unit-test
 # your better_evaluate function.
 if False:
-    board_tuples = (( 0,0,0,0,0,0,0 ),
-                    ( 0,0,0,0,0,0,0 ),
-                    ( 0,0,0,0,0,0,0 ),
-                    ( 0,2,2,1,1,2,0 ),
+    board_tuples = (( 0,0,2,2,2,0,0 ),
+                    ( 0,0,2,1,2,0,0 ),
+                    ( 0,0,1,1,2,0,0 ),
+                    ( 0,2,2,2,1,2,0 ),
                     ( 0,2,1,2,1,2,0 ),
                     ( 2,1,2,1,1,1,0 ),
                     )
@@ -152,7 +237,7 @@ your_player = lambda board: run_search_function(board,
 def run_test_game(player1, player2, board):
     assert isinstance(globals()[board], ConnectFourBoard), "Error: can't run a game using a non-Board object!"
     return run_game(globals()[player1], globals()[player2], globals()[board])
-    
+
 def run_test_search(search, board, depth, eval_fn):
     assert isinstance(globals()[board], ConnectFourBoard), "Error: can't run a game using a non-Board object!"
     return globals()[search](globals()[board], depth=depth,
@@ -165,16 +250,13 @@ def run_test_tree_search(search, board, depth):
                              eval_fn=tree_searcher.tree_eval,
                              get_next_moves_fn=tree_searcher.tree_get_next_move,
                              is_terminal_fn=tree_searcher.is_leaf)
-    
+
 ## Do you want us to use your code in a tournament against other students? See
 ## the description in the problem set. The tournament is completely optional
 ## and has no effect on your grade.
-COMPETE = (None)
+COMPETE = (True)
 
 ## The standard survey questions.
-HOW_MANY_HOURS_THIS_PSET_TOOK = ""
-WHAT_I_FOUND_INTERESTING = ""
-WHAT_I_FOUND_BORING = ""
-NAME = ""
-EMAIL = ""
-
+HOW_MANY_HOURS_THIS_PSET_TOOK = "1"
+WHAT_I_FOUND_INTERESTING = "1"
+WHAT_I_FOUND_BORING = "1"
